d45553ec9dc284b88f6bb024b532994412534da2
lkx <2799679809@qq.com>
2026-04-01 21:33:36 +0800

    对week1中代码添加了一些注释

--- week1/src/vector_add.cu
+++ week1/src/vector_add.cu
@@ -5,6 +5,7 @@
 
 // Check every CUDA API call and stop immediately on failure.
 // This avoids silent errors during early learning.
+// 检查调用CUDA API是否有错误，如果有会及时报错
 #define CHECK_CUDA(call)                                                        \
     do {                                                                        \
         cudaError_t err = (call);                                               \
@@ -16,11 +17,14 @@
     } while (0)
 
 // GPU kernel: each thread computes one output element.
+// __global__ 表示GPU执行
 __global__ void vectorAddKernel(const float* a, const float* b, float* c, int n) {
     // Compute the global thread index.
+    // 算出线程在全局中的编号
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
     // Guard against out-of-range threads in the last block.
+    // 防止线程编号超出数组下标
     if (idx < n) {
         c[idx] = a[idx] + b[idx];
     }
@@ -28,6 +32,7 @@ __global__ void vectorAddKernel(const float* a, const float* b, float* c, int n)
 
 int main() {
     // Input length. 2^20 is large enough for a simple parallel example.
+    // CPU 准备数据 → 拷到 GPU → GPU 并行计算 → 结果拷回 CPU → CPU 验证
     const int n = 1 << 20;
     const size_t bytes = static_cast<size_t>(n) * sizeof(float);
 
@@ -43,25 +48,31 @@ int main() {
     }
 
     // Device pointers stored in GPU memory.
+    // 此时还没有分配内存
     float* d_a = nullptr;
     float* d_b = nullptr;
     float* d_c = nullptr;
 
     // Allocate memory on the GPU.
+    // 为这三个指针分配GPU中的内存
     CHECK_CUDA(cudaMalloc(&d_a, bytes));
     CHECK_CUDA(cudaMalloc(&d_b, bytes));
     CHECK_CUDA(cudaMalloc(&d_c, bytes));
 
     // Copy inputs from CPU memory to GPU memory.
+
     CHECK_CUDA(cudaMemcpy(d_a, h_a.data(), bytes, cudaMemcpyHostToDevice));
     CHECK_CUDA(cudaMemcpy(d_b, h_b.data(), bytes, cudaMemcpyHostToDevice));
 
     // Use 256 threads per block.
     // gridSize is the number of blocks needed to cover n elements.
+    // 每个block中有256个线程
     const int blockSize = 256;
+    // 每个grid中需要有多少个block
     const int gridSize = (n + blockSize - 1) / blockSize;
 
     // Launch the kernel with CUDA's <<<grid, block>>> syntax.
+    // 在GPU上启动这个多的线程，让他们同时执行vectorAddKernel函数
     vectorAddKernel<<<gridSize, blockSize>>>(d_a, d_b, d_c, n);
 
     // Check launch errors and wait for GPU execution to finish.
